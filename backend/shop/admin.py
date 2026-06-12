4d29632c9064aab26286c77b70dbfbc967670d64
strek-o <strek.oliwier@gmail.com>
2026-06-12 21:24:35 +0200

    feat: register models in Django admin

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "origin", "roast_level", "price_per_kg", "is_available")
+    list_filter = ("roast_level", "is_available")
+    search_fields = ("name", "origin")
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status", "created_at")
+    list_filter = ("status",)
+    inlines = [OrderItemInline]
